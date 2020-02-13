8272a198052e5b73126721f6daae188409084ffb
eric.dilli <eric.dilli@GCCYJQAWJ1WK.local>
2020-02-13 09:13:05 -0500

    HTML

---
+++ scriptfeb7.js
@@ -0,0 +1,11 @@
+for (i = 0; i < 100; i = i + i ) {
+
+	console.log(i);
+}
+
+list = [1,5,4,6,4,6,6,6,4,6,4,6,4,7,5,7,4,7,7,5];
+
+sum = 0 
+for (i = 0; i < list.length; i = i + 1) {
+	sum = sum + list[i];
+}
