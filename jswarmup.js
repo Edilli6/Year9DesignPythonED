24d3b4e052122d0e59aac6935cf21979a864c688
eric.dilli <eric.dilli@GCCYJQAWJ1WK.local>
2020-02-19 13:57:55 -0500

    Update

---
+++ jswarmup.js
@@ -0,0 +1,7 @@
+console.log(
+  [4, 7, 9, 10, 11].reduce((a, b) => a + b, 0)
+)
+
+console.log(
+  [].reduce((a, b) => a + b, 0)
+)
---
+++ loops.js
@@ -0,0 +1,11 @@
+list = [4,7,9,10,11]
+
+sum = 0
+
+for(var i = 0; i < list.length; i++) {
+	if(list[i] > 5)
+	sum += list[i]
+}
+
+console.log (sum);
+
